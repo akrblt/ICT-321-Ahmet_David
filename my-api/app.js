764c1cf8e6db578856f57a0602fe80b80abf254d
akrblt <a.karabulut070@gmail.com>
2025-11-27 09:09:33 +0100

    feat : add parameters in app.js

--- my-api/app.js
+++ my-api/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var pizzasRouter = require('./routes/pizzas');
+var ingredientsRouter = require('./routes/ingredients');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/pizzas', pizzasRouter);
+app.use('/ingredients', ingredientsRouter);
 
 module.exports = app;
