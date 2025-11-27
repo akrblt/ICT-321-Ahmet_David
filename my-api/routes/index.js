65d24e6edb5877e3b839e99319ca8ff0f144d33e
akrblt <a.karabulut070@gmail.com>
2025-11-27 09:10:18 +0100

    feat: add routes ingredients

--- my-api/routes/index.js
+++ my-api/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+var pizzasRouter = require('./pizzas');
+var ingredientsRouter = require('./ingredients');
+
+router.use('/pizzas', pizzasRouter);
+router.use('/ingredients', ingredientsRouter)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'david' });
+    res.render('index', { title: 'David & Ahmet' });
 });
 
 module.exports = router;
