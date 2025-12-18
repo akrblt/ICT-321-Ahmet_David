8ce9b0ac56eebd0983404bf031c77186d8485952
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2025-12-18 08:52:37 +0100

    refactor:create pizza du jour

--- my-api/routes/pizza-du-jour.js
+++ my-api/routes/pizza-du-jour.js
@@ -2,7 +2,7 @@ import express from 'express';
 import db from '../db/db.js';
 const router = express.Router();
 
-/* Get pizza of the day */
+/* Read pizza du jour */
 /**
  * @openapi
  * /pizza-du-jour:
@@ -29,6 +29,76 @@ router.get('/', async (req, res, next) => {
             'ON pr.id_pizza = pi.id_pizza \n' +
             'WHERE CURDATE() BETWEEN pr.date_start AND pr.date_finish;');
         res.status(200).json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database error');
+    }
+});
+
+// Create pizza du jour
+/**
+ * @openapi
+ * /pizzas/pizzadujour/create:
+ *   post:
+ *     summary: create an offer for a pizza.
+ *     description: post an offer with a start and finish date for a pizza.
+ *     parameters:
+ *       - name: id_pizza
+ *         in: body
+ *         required: true
+ *         schema:
+ *              type: string
+ *              description: returns pizza matching name
+ *       - name: date_start
+ *         in: body
+ *         required: false
+ *         schema:
+ *              type: string
+ *              description: returns pizza matching description
+ *       - name: date_finish
+ *         in: body
+ *         required: true
+ *         schema:
+ *              type: float
+ *              description: returns pizza matching price
+ *       - name: rabais
+ *         in: body
+ *         required: false
+ *         schema:
+ *              type: string
+ *              description: returns pizza matching image
+ *     responses:
+ *       200:
+ *         description: add the offer for the pizza and return the offer info.
+ *         content:
+ *             application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                    $ref: "#/components/schemas/promotion"
+ *       400:
+ *         description: exception describing all required fields
+ *       500:
+ *         description: system exception describing the error.
+ */
+router.post('/pizzadujour/create', async (req, res, next) => {
+    const {id_pizza, date_start, date_finish, rabais} = req.body;
+    if (!id_pizza || !date_start || !date_finish || rabais == null) {
+        return res.status(400).json({
+            error: "Tous les champs sont obligatoires (id_pizza, date_start, date_finish, rabais).",
+        });
+    }
+    try {
+
+        const[result] = await db.query('INSERT INTO promotion (id_pizza,date_start ,date_finish, rabais) VALUES (?, ?, ?, ?)', [ id_pizza, date_start, date_finish, rabais ]);
+
+        return res.status(201).json({
+            id: result.insertId,
+            id_pizza,
+            date_start,
+            date_finish,
+            rabais
+        });
     } catch (err) {
         console.error(err);
         res.status(500).send('Database error');
