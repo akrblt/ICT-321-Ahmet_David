a981d45b090258d6f9d2b08288ce65b53789137e
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-01-08 08:40:19 +0100

    feat:pizza du jour router

--- my-api/routes/index.js
+++ my-api/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import pizzasRouter from './pizzas.js';
 import ingredientsRouter from './ingredients.js';
+import pizzaDuJourRouter from "./pizza-du-jour.js";
 
 const router = express.Router();
 
 router.use('/pizzas', pizzasRouter);
 router.use('/ingredients', ingredientsRouter);
+router.use('/pizza-du-jour', pizzaDuJourRouter);
 
 // Home Page //
 router.get('/', (req, res) => {
